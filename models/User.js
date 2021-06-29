88934addd27ab9e456d27d9f73e0315381f1d5a9
Zura Kurtanidze <zura.kurta@gmail.com>
2021-06-29 19:31:42 +0400

    :label: Create User model.

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  ip: String,
+  visitedLinks: [{ type: Schema.Types.ObjectId, ref: "Url" }]
+})
+
+function validateUser(user) {
+  const schema = Joi.object({
+    ip: Joi.string(),
+    visitedLinks: Joi.array()
+  })
+  return schema.validate(user);
+}
+
+const User = mongoose.model("User", userSchema);
+
+exports.User = User;
+exports.validate = validateUser;
+

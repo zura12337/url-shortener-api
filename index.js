2532528d4c15cfad8ccb068f7c56049d15f7ac0f
Zura Kurtanidze <zura.kurta@gmail.com>
2021-06-30 13:43:46 +0400

    :sparkles: Use cors.

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const http = require('http').Server(app);
-const url = require("./routes/url")
+const http = require("http").Server(app);
+const url = require("./routes/url");
+var cors = require("cors");
 
-mongoose.connect('mongodb://127.0.0.1', {
+mongoose.connect("mongodb://127.0.0.1", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   auth: {
     user: "admin",
-    password: "admin"
+    password: "admin",
   },
   dbName: "url-shortener",
-})
+});
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", url);
 
-const port = 4000
+const port = 4000;
 const server = http.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
-})
+});

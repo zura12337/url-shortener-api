e42b3754c6a303ea22d5ff8568746e888583e3ed
Zura Kurtanidze <zura.kurta@gmail.com>
2021-07-06 22:00:18 +0400

    :sparkles: Create middleware for authentication.

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const { User } = require('../models/User');
+
+async function auth(req, res, next) {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+  req.user = ip;
+
+  let user = await User.findOne({ ip });
+  if (!user) {
+    user = new User({
+      ip,
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = auth;
